53b2b99580440b39faed5d1930e714140ae398c4
RakhmonovDilmurod <dilmurodrakhmonov51@gmail.com>
2023-12-15 23:45:56 +0500

    Updated

---
+++ build/CMakeFiles/search_engine_4.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/search_engine_4.dir/main.cpp.obj"
+  "CMakeFiles/search_engine_4.dir/main.cpp.obj.d"
+  "libsearch_engine_4.dll.a"
+  "search_engine_4.exe"
+  "search_engine_4.exe.manifest"
+  "search_engine_4.pdb"
+  "search_engine_4[1]_tests.cmake"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/search_engine_4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
