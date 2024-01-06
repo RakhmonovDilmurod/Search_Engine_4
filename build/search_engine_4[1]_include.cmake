088fbc903b7a2ef97d2272e4ea54fb74f9fa9888
RakhmonovDilmurod <dilmurodrakhmonov51@gmail.com>
2024-01-06 19:35:12 +0500

    Updated

---
+++ build/search_engine_4[1]_include.cmake
@@ -0,0 +1,5 @@
+if(EXISTS "C:/Users/HP/OneDrive/Documents/GitHub/Search_Engine_4/build/search_engine_4[1]_tests.cmake")
+  include("C:/Users/HP/OneDrive/Documents/GitHub/Search_Engine_4/build/search_engine_4[1]_tests.cmake")
+else()
+  add_test(search_engine_4_NOT_BUILT search_engine_4_NOT_BUILT)
+endif()
