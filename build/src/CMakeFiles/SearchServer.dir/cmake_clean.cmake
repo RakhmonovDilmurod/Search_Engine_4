6377075daeef448b69c5de599c5d47673a72b5e3
RakhmonovDilmurod <dilmurodrakhmonov51@gmail.com>
2024-01-19 20:33:54 +0500

    Updated

---
+++ build/src/CMakeFiles/SearchServer.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "C:/Users/HP/OneDrive/Documents/GitHub/Search_Engine_4/SearchServer.exe"
+  "C:/Users/HP/OneDrive/Documents/GitHub/Search_Engine_4/SearchServer.exe.manifest"
+  "C:/Users/HP/OneDrive/Documents/GitHub/Search_Engine_4/SearchServer.pdb"
+  "CMakeFiles/SearchServer.dir/ConverterJSON.cpp.obj"
+  "CMakeFiles/SearchServer.dir/ConverterJSON.cpp.obj.d"
+  "CMakeFiles/SearchServer.dir/InvertedIndex.cpp.obj"
+  "CMakeFiles/SearchServer.dir/InvertedIndex.cpp.obj.d"
+  "CMakeFiles/SearchServer.dir/SearchServer.cpp.obj"
+  "CMakeFiles/SearchServer.dir/SearchServer.cpp.obj.d"
+  "CMakeFiles/SearchServer.dir/main.cpp.obj"
+  "CMakeFiles/SearchServer.dir/main.cpp.obj.d"
+  "libSearchServer.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SearchServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
