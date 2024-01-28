298913b33b83001ab72be55cb674979e42d2abef
Sergey Masuryan <s.masuryan@visionlabs.ru>
2024-01-28 18:59:11 +0300

    Unnecessary

--- build/nlohmann_json/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/HP/OneDrive/Documents/GitHub/Search_Engine_4/nlohmann_json
-# Build directory: C:/Users/HP/OneDrive/Documents/GitHub/Search_Engine_4/build/nlohmann_json
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
