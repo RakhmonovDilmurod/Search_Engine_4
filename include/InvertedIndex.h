c84949ac57630d4d8b4db20734bb8dd994168e7b
RakhmonovDilmurod <dilmurodrakhmonov51@gmail.com>
2024-02-12 13:40:51 +0500

    Updated

--- include/InvertedIndex.h
+++ include/InvertedIndex.h
@@ -24,30 +24,38 @@ struct Entry {
 };
 class InvertedIndex {
 public:
+    ConverterJSON conv;
     InvertedIndex() = default;
+    std::vector<std::string> docs = conv.GetTextDocuments(); // список содержимого документов
+    std::map<std::string, std::vector<Entry>> freq_dictionary; // частотный словарь
 
+    
    void UpdateDocumentBase(std::vector<std::string> input_docs) {
-       docs.clear();
-       freq_dictionary.clear();
-       for (size_t doc_id = 0; doc_id < input_docs.size(); ++doc_id) {
-           const std::string& document_content = input_docs[doc_id];
-           docs.push_back(document_content);
-           std::istringstream iss(document_content);
-           std::string word;
-           while (iss >> word) {
-               auto it = freq_dictionary.find(word);
-               if (it != freq_dictionary.end()) {
-                   for (auto& entry : it->second) {
-                       if (entry.doc_id == doc_id) {
-                           entry.count++;
-                           break;
-                       }
+    freq_dictionary.clear();
+    size_t doc_id = docs.size();
+    for (const std::string& document_content : input_docs) {
+        docs.push_back(document_content);
+        std::istringstream iss(document_content);
+        std::string word;
+        while (iss >> word) {
+            auto it = freq_dictionary.find(word);
+            if (it != freq_dictionary.end()) {
+                for (auto& entry : it->second) {
+                    if (entry.doc_id == doc_id) {
+                        entry.count++;
+                        break;
                     }
-                } else {
-                   freq_dictionary[word] = { {doc_id, 1} };
+                }
+            } else {
+                freq_dictionary[word] = { {doc_id, 1} };
             }
         }
+        doc_id++;
     }
+}
+
+    auto getFreqDictionary() {
+        return freq_dictionary;
 }
 
     std::vector<Entry> GetWordCount(const std::string& word) {
@@ -57,14 +65,5 @@ public:
            result = it->second;
     }
        return result;
-}
-
-    auto getFreqDictionary() {
-        return freq_dictionary;
-}
-
-
-public:
-    std::vector<std::string> docs; // список содержимого документов
-    std::map<std::string, std::vector<Entry>> freq_dictionary; // частотный словарь
+  }
 };
