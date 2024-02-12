71c087a56dece70dc98288f5d750849013863c32
RakhmonovDilmurod <dilmurodrakhmonov51@gmail.com>
2024-02-12 12:30:46 +0500

    Updated

--- include/InvertedIndex.h
+++ include/InvertedIndex.h
@@ -26,33 +26,34 @@ class InvertedIndex {
 public:
     InvertedIndex() = default;
 
-    void UpdateDocumentBase(std::vector<std::string> input_docs){
-        docs = input_docs;
-        freq_dictionary.clear();
-        for (size_t doc_id = 0; doc_id < input_docs.size(); ++doc_id) {
-            std::map<std::string, int> word_count;
-            std::istringstream iss(input_docs[doc_id]);
-            std::string word;
-            while (iss >> word) {
-                word_count[word]++;
-            }
-            for (const auto& p : word_count) {
-                freq_dictionary[p.first].push_back({doc_id,size_t(p.second)});
-            }
+   void UpdateDocumentBase(std::vector<std::string> input_docs) {
+    docs.clear();
+    freq_dictionary.clear();
+
+    for (size_t doc_id = 0; doc_id < input_docs.size(); ++doc_id) {
+        const std::string& document_content = input_docs[doc_id];
+        docs.push_back(document_content);
+        std::istringstream iss(document_content);
+        std::string word;
+        while (iss >> word) {
+            freq_dictionary[word].push_back({doc_id, 1});
         }
     }
+}
 
     std::vector<Entry> GetWordCount(const std::string& word){
         std::vector<Entry> result;
-        auto it = freq_dictionary.find(word);
+    auto it = freq_dictionary.find(word);
         if (it != freq_dictionary.end()) {
-            result = it->second;
-        }
-        return result;
-    }
+            result = it->second; } 
+    return result;
+}
+
     auto getFreqDictionary() {
         return freq_dictionary;
-    }
+}
+
+
 public:
     std::vector<std::string> docs; // список содержимого документов
     std::map<std::string, std::vector<Entry>> freq_dictionary; // частотный словарь
