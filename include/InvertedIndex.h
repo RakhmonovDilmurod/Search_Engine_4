8d38692a72ded98d12f0262a0e17178c49d14395
RakhmonovDilmurod <dilmurodrakhmonov51@gmail.com>
2024-02-21 18:10:13 +0500

    Updated

--- include/InvertedIndex.h
+++ include/InvertedIndex.h
@@ -1,88 +1,79 @@
 #pragma once
 #include <iostream>
 #include <vector>
-#include <map>
 #include <string>
-#include <algorithm>
-#include <sstream>
+#include <map>
 #include <mutex>
 #include <thread>
+#include <string>
+#include "ConverterJSON.h"
 
-struct Entry{
+struct Entry {
     size_t doc_id, count;
-
+    // Данный оператор необходим для проведения тестовых сценариев
     bool operator ==(const Entry& other) const {
-        return (doc_id == other.doc_id && count == other.count);
+        return (doc_id == other.doc_id &&
+                count == other.count);
     }
-}; 
+};
 
 class InvertedIndex {
 public:
+
     InvertedIndex() = default;
-
-    std::vector<std::string> docs;
-    std::map<std::string,std::vector<Entry>> freq_dictionary;
-    
-
-    std::string wordCleaning(std::string word) {
-        while ((word.back() >= ' ' && word.back() <= '/') || (word.back() >= ':' && word.back() <= '@')
-               || (word.back() >= '[' && word.back() <= '`') || (word.back() >= '{' && word.back() <= '~')) {
-            word.erase(word.end() - 1);
-        }
-        while ((word.front() >= ' ' && word.front() <= '/') || (word.front() >= ':' && word.front() <= '@')
-               || (word.front() >= '[' && word.front() <= '`') || (word.front() >= '{' && word.front() <= '~')) {
-            word.erase(0, 1);
-        }
-        return word;
-    }    
-
-    void convertDocumenttoDatabase(const std::string& txt, size_t num) {
-        std::mutex freqAccess;
-        std::map<std::string,size_t> words;
-        std::istringstream iss(txt);
-        std::string buf;
-        while (!iss.eof()) {
-            iss >> buf;
-            buf = wordCleaning(buf);
-            if (words.count(buf) == 0) {
-                words.insert(std::pair<std::string,size_t>(buf, 1));
-            } else {
-                words[buf] += 1;
-            }
-        }
-        for (auto & word : words) {
-            freqAccess.lock();
-            if (freq_dictionary.count(word.first) == 0) {
-                freq_dictionary[word.first] = std::vector<Entry>{Entry{num, word.second}};
-            } else {
-                freq_dictionary[word.first].push_back(Entry{num, word.second});
-            }
-            freqAccess.unlock();
-        }
-    }   
- 
+    std::vector<std::string> docs;// список содержимого документов
     void UpdateDocumentBase(const std::vector<std::string>& input_docs) {
-        docs = input_docs;
         std::vector<std::thread> threads;
-        for (int i = 0; i < input_docs.size(); i++) {
-            threads.emplace_back(&InvertedIndex::convertDocumenttoDatabase, this, docs[i], i);
+        for (size_t i = 0; i < input_docs.size(); ++i) {
+            threads.emplace_back(&InvertedIndex::IndexDocument, this, input_docs[i], i);
         }
-        for (int i = 0; i < input_docs.size(); i++) {
-            threads[i].join();
+        for (auto& thread : threads) {
+            thread.join();
         }
     }
 
-    auto getFreqDictionary() {
-        return freq_dictionary;
-    }
-
-    std::vector<Entry> GetWordCount(const std::string& word) const {
-        std::string lowercase_word = word;
-        std::transform(lowercase_word.begin(), lowercase_word.end(), lowercase_word.begin(), ::tolower);
-        auto it = freq_dictionary.find(lowercase_word);
-        if (it != freq_dictionary.end()) {
-            return it->second;
+    std::vector<Entry> GetWordCount(const std::string& word) {
+        std::mutex mutex_;
+        std::lock_guard<std::mutex> lock(mutex_);
+        if (freq_dictionary.find(word) != freq_dictionary.end()) {
+            return freq_dictionary[word];
         }
         return {};
+    }
+ 
+private:
+    std::map<std::string, std::vector<Entry>> freq_dictionary; // частотный словарь
+    void IndexDocument(const std::string& doc, size_t doc_id){ 
+        std::mutex mutex_;
+        std::map<std::string, size_t> word_count;
+        std::istringstream iss(doc);
+        std::string word;
+        while (iss >> word) {
+            word = WordCleaning(word);
+            ++word_count[word];
+        }
+
+        std::lock_guard<std::mutex> lock(mutex_);
+        for (const auto& [word, count] : word_count) {
+            if (freq_dictionary.find(word) == freq_dictionary.end()) {
+                freq_dictionary[word] = {{doc_id, count}};
+            } else {
+                freq_dictionary[word].emplace_back(Entry{doc_id, count});
+            }
+        }
+    }
+   
+    std::string WordCleaning(std::string word) {
+        while (!word.empty() && !isalnum(word.front())) {
+            word.erase(word.begin());
+        }
+        while (!word.empty() && !isalnum(word.back())) {
+            word.pop_back();
+        }
+        return word;
+    }
+public:
+    auto getFreqDictionary() {
+        return freq_dictionary;
     }
 };
