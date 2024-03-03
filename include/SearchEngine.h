a0bda0d941d00f9ec1066528ce3ee1cfd8e911e5
RakhmonovDilmurod <dilmurodrakhmonov51@gmail.com>
2024-03-04 00:58:41 +0500

    Updated

--- include/SearchEngine.h
+++ include/SearchEngine.h
@@ -33,46 +33,48 @@ private:
     std::vector<std::string> requestsInput = converter.GetRequests();
     
 public:
-    std::vector<std::vector<RelativeIndex>> search(const std::vector<std::string>& queries_input) {
-        std::vector<std::vector<RelativeIndex>> result = {};
-        for (const auto& i : requestsInput) {
-            std::map<std::string, std::pair<size_t, int>> wordRelevance;
-            std::vector<int> absoluteRelevance = {};
-            std::vector<RelativeIndex> relativeRelevance;
-            result.emplace_back();
-            std::istringstream iss(i);
-            std::string buf;
-            std::vector<std::string> uniqueWords = {};
-            while (!iss.eof()) {
-                iss >> buf;
-                buf = index.WordCleaning(buf);
-                if (std::count(uniqueWords.begin(), uniqueWords.end(), buf) == 0) {
-                    uniqueWords.push_back(buf);
+std::vector<std::vector<RelativeIndex>> search(const std::vector<std::string>& queries_input) {
+    std::vector<std::vector<RelativeIndex>> result = {};
+    for (const auto& i : requestsInput) {
+        std::map<std::string, std::pair<size_t, int>> wordRelevance;
+        std::vector<float> absoluteRelevance = {};
+        std::vector<RelativeIndex> relativeRelevance;
+        result.emplace_back();
+        std::istringstream iss(i);
+        std::string buf;
+        std::vector<std::string> uniqueWords = {};
+        while (!iss.eof()) {
+            iss >> buf;
+            buf = index.WordCleaning(buf);
+            if (std::count(uniqueWords.begin(), uniqueWords.end(), buf) == 0) {
+                uniqueWords.push_back(buf);
+            }
+        }
+        for (const auto& j : uniqueWords) {
+            for (size_t k = 0; k < converter.GetTextDocuments().size(); k++) {
+                if (freq_dictionary.count(j) != 0) {
+                    wordRelevance[j] = {k, freq_dictionary[j][k].count};
+                } else {
+                    wordRelevance[j] = {k, 0};
                 }
             }
-            for (const auto& j : uniqueWords) {
-                for (size_t k = 0; k < converter.GetTextDocuments().size(); k++) {
-                    if (freq_dictionary.count(j) != 0) {
-                        wordRelevance[j] = {k, freq_dictionary[j][k].count};
-                    } else {
-                        wordRelevance[j] = {k, 0};
-                    }
-                }
-            }
-            for (size_t j = 0; j < converter.GetTextDocuments().size(); j++) {
-                int currentAbsRelevence = 0;
-                for (const auto& k : wordRelevance) {
-                    if (k.second.first == j) {
-                        currentAbsRelevence += k.second.second;
-                    }
-                }
-                absoluteRelevance.push_back(currentAbsRelevence);
-            }
-            for (int j = 0; j < absoluteRelevance.size(); j++) {
-               relativeRelevance.push_back(RelativeIndex{(size_t)j, static_cast<float>(absoluteRelevance[j]) / *std::max_element(absoluteRelevance.begin(), absoluteRelevance.end())});
-            }
-           result.push_back(relativeRelevance);
-          }
-          return result;
-     }
+        }
+        std::vector<int> documentFrequencies(converter.GetTextDocuments().size(), 0);
+        for (const auto& j : wordRelevance) {
+            documentFrequencies[j.second.first] += j.second.second;
+        }
+        float maxFrequency = *std::max_element(documentFrequencies.begin(), documentFrequencies.end());
+        if (maxFrequency == 0) {
+            maxFrequency = 1;
+        }
+        for (size_t j = 0; j < converter.GetTextDocuments().size(); j++) {
+            absoluteRelevance.push_back(documentFrequencies[j] * 1.0 / maxFrequency);
+        }
+        for (size_t j = 0; j < absoluteRelevance.size(); j++) {
+            relativeRelevance.push_back(RelativeIndex{(size_t)j, absoluteRelevance[j]});
+        }
+        result.push_back(relativeRelevance);
+    }
+    return result;
+  }
 };
